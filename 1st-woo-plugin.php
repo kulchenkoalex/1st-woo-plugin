2a65496a34d548d2d71ddad1901b842f496fa9e5
kulchenkoalex <alexej07@bk.ru>
2017-04-29 00:42:17 +0300

    Новая почта

--- 1st-woo-plugin.php
+++ 1st-woo-plugin.php
@@ -43,5 +43,44 @@ if ( ! defined( 'ABSPATH' ) ) {
         return $methods;
     }
     add_filter( 'woocommerce_shipping_methods', 'add_tutsplus_shipping_method' );
+
+    function novaya_pochta_shipping_method_init() {
+        require_once dirname(__FILE__) . '/WC_Novaya_Pochta_Shipping_Method.php';
+    }
+    add_action( 'woocommerce_shipping_init', 'novaya_pochta_shipping_method_init' );
+    function add_novaya_pochta_shipping_method( $methods ) {
+        $methods['novaya_pochta_shipping_method'] = 'WC_Novaya_Pochta_Shipping_Method';
+        return $methods;
+    }
+    add_filter( 'woocommerce_shipping_methods', 'add_novaya_pochta_shipping_method' );
+    function tutsplus_validate_order( $posted )   {
+        $packages = WC()->shipping->get_packages();
+        $chosen_methods = WC()->session->get( 'chosen_shipping_methods' );
+        if( is_array( $chosen_methods ) && in_array( 'tutsplus', $chosen_methods ) ) {
+            foreach ( $packages as $i => $package ) {
+                if ( $chosen_methods[ $i ] != "tutsplus" ) {
+                    continue;
+                }
+                $TutsPlus_Shipping_Method = new WC_Novaya_Pochta_Shipping_Method();
+                $weightLimit = (int) $TutsPlus_Shipping_Method->settings['weight'];
+                $weight = 0;
+                foreach ( $package['contents'] as $item_id => $values )
+                {
+                    $_product = $values['data'];
+                    $weight = $weight + $_product->get_weight() * $values['quantity'];
+                }
+                $weight = wc_get_weight( $weight, 'kg' );
+                if( $weight > $weightLimit ) {
+                    $message = sprintf( __( 'Sorry, %d kg exceeds the maximum weight of %d kg for %s', 'tutsplus' ), $weight, $weightLimit, $TutsPlus_Shipping_Method->title );
+                    $messageType = "error";
+                    if( ! wc_has_notice( $message, $messageType ) ) {
+                        wc_add_notice( $message, $messageType );
+                    }
+                }
+            }
+        }
+    }
+    add_action( 'woocommerce_review_order_before_cart_contents', 'tutsplus_validate_order' , 10 );
+    add_action( 'woocommerce_after_checkout_validation', 'tutsplus_validate_order' , 10 );
     
 }
